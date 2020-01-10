cbb3ab02099dd9077c5cd0bc77e24352420442fd
Student <example@example.com>
2020-01-10 09:15:46 -0800

    tests updated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,14 @@
-require 'rspec'
-require 'pg'
-require 'book'
-require 'patron'
-require 'pry'
+require "volunteer"
+require "project"
+require "rspec"
+require "pry"
+require "pg"
 
-DB = PG.connect({:dbname => 'library_test'})
+DB = PG.connect({:dbname => 'volunteer_tracker_test'})
 
 RSpec.configure do |config|
   config.after(:each) do
-    DB.exec("DELETE FROM books *;")
-    DB.exec("ALTER SEQUENCE books_id_seq RESTART WITH 1;")
-    DB.exec("DELETE FROM patrons *;")
-    DB.exec("ALTER SEQUENCE patrons_id_seq RESTART WITH 1;")
-    DB.exec("DELETE FROM authors *;")
-    DB.exec("ALTER SEQUENCE authors_id_seq RESTART WITH 1;")
+    DB.exec('DELETE FROM volunteers *;')
+    DB.exec('DELETE FROM projects *;')
   end
 end
